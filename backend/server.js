9fda3862b91e3ea8315ee1d80ec70525d5a7e9f2
Maxerza <kuliazc11a@gmail.com>
2026-02-03 20:38:53 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 
 app.use(express.static(path.join(__dirname, "..", "frontend")));
